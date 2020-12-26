38c0894ae4220c588488557ec5ca3ecf62aab977
weijhuang <weii41392@gmail.com>
2020-12-26 11:55:58 +0800

    Add d5 train script

--- train.sh
+++ train.sh
@@ -1,10 +1,21 @@
 # train from d6 pretrained weight provided by zylo117
+# CUDA_VISIBLE_DEVICES=0,1,2,3 \
+# python3 train.py \
+# --compound_coef 6 \
+# --batch_size 4 \
+# --load_weights models/efficientdet-d6.pth \
+# --debug True
+
+# Disable visualizations to train faster
+# CUDA_VISIBLE_DEVICES=0,1,2,3 \
+# python3 train.py \
+# --compound_coef 6 \
+# --batch_size 4 \
+# --load_weights last \
+
+# train from d5 pretrained weight provided by zylo117
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python3 train.py \
---compound_coef 6 \
+--compound_coef 5 \
 --batch_size 4 \
---lr 1e-4 \
---optim adamw \
---num_epochs 500 \
---load_weights models/efficientdet-d6.pth \
---debug True
+--load_weights models/efficientdet-d5.pth
