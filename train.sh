0919385c5b265c3b0ea00308afaeae6b9f2c5c7b
weijhuang <weii41392@gmail.com>
2020-12-30 16:56:33 +0800

    Add new train script

--- train.sh
+++ train.sh
@@ -29,13 +29,58 @@
 # --lr 1e-4 \
 # --load_weights last
 
-# Try
+# S1
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4 \
+# python3 train.py \
+# --compound_coef 7 \
+# --load_weights models/efficientdet-d7.pth \
+# --batch_size 10 \
+# --force_input_size 512 \
+# --aug_prob 0 \
+# --lr 5e-4 \
+# --num_epoch 10
+
+# S2
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4 \
+# python3 train.py \
+# --compound_coef 7 \
+# --load_weights logs/global-wheat-detection/efficientdet-d7_10_2959.pth \
+# --batch_size 10 \
+# --force_input_size 512 \
+# --aug_prob 0.5 \
+# --lr 3e-4 \
+# --num_epoch 50
+
+# S1
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4 \
+# python3 train.py \
+# --compound_coef 7 \
+# --load_weights models/efficientdet-d7.pth \
+# --batch_size 10 \
+# --force_input_size 512 \
+# --aug_prob 0 \
+# --lr 3e-4 \
+# --num_epoch 20 \
+
+# S2
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4 \
+# python3 train.py \
+# --compound_coef 7 \
+# --load_weights last \
+# --batch_size 10 \
+# --force_input_size 512 \
+# --aug_prob 0.1 \
+# --lr 3e-4 \
+# --num_epoch 80 \
+
+# S3
 CUDA_VISIBLE_DEVICES=0,1,2,3,4 \
 python3 train.py \
+--train_split 1 \
 --compound_coef 7 \
---load_weights models/efficientdet-d7.pth \
---batch_size 15 \
+--load_weights last \
+--batch_size 10 \
 --force_input_size 512 \
---aug_prob 0 \
---lr 5e-4 \
---num_epoch 30
+--aug_prob 0.1 \
+--lr 3e-4 \
+--num_epoch 100 \
