5eb158008390109b7d8380fcbdc3235c97d612c4
weijhuang <weii41392@gmail.com>
2021-01-02 21:51:52 +0800

    Update default settings

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 """
 Simple Inference Script of EfficientDet-Pytorch
 """
+import os
 import time
 import torch
 from torch.backends import cudnn
@@ -15,16 +16,16 @@ import numpy as np
 from efficientdet.utils import BBoxTransform, ClipBoxes
 from utils.utils import preprocess, invert_affine, postprocess, STANDARD_COLORS, standard_to_bgr, get_index_label, plot_one_box
 
-compound_coef = 5
-force_input_size = 896  # set None to use default size
-img_path = 'datasets/global-wheat-detection/test/2fd875eaa.jpg'
+compound_coef = 7
+force_input_size = 512  # set None to use default size
+root = 'datasets/global-wheat-detection/test'
 
 # replace this part with your project's anchor config
 anchor_ratios = [(1.0, 1.0), (1.4, 0.7), (0.7, 1.4)]
 anchor_scales = [2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)]
 
-threshold = 0.0
-iou_threshold = 0.0
+threshold = 0.5
+iou_threshold = 0.7
 
 use_cuda = True
 use_float16 = False
@@ -38,18 +39,10 @@ color_list = standard_to_bgr(STANDARD_COLORS)
 # tf bilinear interpolation is different from any other's, just make do
 input_sizes = [512, 640, 768, 896, 1024, 1280, 1280, 1536, 1536]
 input_size = input_sizes[compound_coef] if force_input_size is None else force_input_size
-ori_imgs, framed_imgs, framed_metas = preprocess(img_path, max_size=input_size)
-
-if use_cuda:
-    x = torch.stack([torch.from_numpy(fi).cuda() for fi in framed_imgs], 0)
-else:
-    x = torch.stack([torch.from_numpy(fi) for fi in framed_imgs], 0)
-
-x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
 
 model = EfficientDetBackbone(compound_coef=compound_coef, num_classes=len(obj_list),
                              ratios=anchor_ratios, scales=anchor_scales)
-model.load_state_dict(torch.load('logs/global-wheat-detection/efficientdet-d5_20_14000.pth', map_location='cpu'))
+model.load_state_dict(torch.load('logs/global-wheat-detection/efficientdet-d7-1/efficientdet-d7_98_26500.pth', map_location='cpu'))
 model.requires_grad_(False)
 model.eval()
 
@@ -58,59 +51,69 @@ if use_cuda:
 if use_float16:
     model = model.half()
 
-with torch.no_grad():
-    features, regression, classification, anchors = model(x)
+for name in os.listdir(root):
+    img_path = os.path.join(root, name)
+    ori_imgs, framed_imgs, framed_metas = preprocess(img_path, max_size=input_size)
 
-    regressBoxes = BBoxTransform()
-    clipBoxes = ClipBoxes()
+    if use_cuda:
+        x = torch.stack([torch.from_numpy(fi).cuda() for fi in framed_imgs], 0)
+    else:
+        x = torch.stack([torch.from_numpy(fi) for fi in framed_imgs], 0)
 
-    out = postprocess(x,
-                      anchors, regression, classification,
-                      regressBoxes, clipBoxes,
-                      threshold, iou_threshold)
+    x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
+    with torch.no_grad():
+        features, regression, classification, anchors = model(x)
 
-def display(preds, imgs, imshow=True, imwrite=False):
-    for i in range(len(imgs)):
-        if len(preds[i]['rois']) == 0:
-            continue
-
-        imgs[i] = imgs[i].copy()
-
-        for j in range(len(preds[i]['rois'])):
-            x1, y1, x2, y2 = preds[i]['rois'][j].astype(np.int)
-            obj = obj_list[preds[i]['class_ids'][j]]
-            score = float(preds[i]['scores'][j])
-            plot_one_box(imgs[i], [x1, y1, x2, y2], label=obj,score=score,color=color_list[get_index_label(obj, obj_list)])
-
-
-        if imshow:
-            cv2.imshow('img', imgs[i])
-            cv2.waitKey(0)
-
-        if imwrite:
-            cv2.imwrite(f'test/img_inferred_d{compound_coef}_this_repo_{i}.jpg', imgs[i])
-
-
-out = invert_affine(framed_metas, out)
-display(out, ori_imgs, imshow=False, imwrite=True)
-
-print('running speed test...')
-with torch.no_grad():
-    print('test1: model inferring and postprocessing')
-    print('inferring image for 10 times...')
-    t1 = time.time()
-    for _ in range(10):
-        _, regression, classification, anchors = model(x)
+        regressBoxes = BBoxTransform()
+        clipBoxes = ClipBoxes()
 
         out = postprocess(x,
-                          anchors, regression, classification,
-                          regressBoxes, clipBoxes,
-                          threshold, iou_threshold)
-        out = invert_affine(framed_metas, out)
+                        anchors, regression, classification,
+                        regressBoxes, clipBoxes,
+                        threshold, iou_threshold)
 
-    t2 = time.time()
-    tact_time = (t2 - t1) / 10
-    print(f'{tact_time} seconds, {1 / tact_time} FPS, @batch_size 1')
+    def display(preds, imgs, imshow=True, imwrite=False):
+        for i in range(len(imgs)):
+            if len(preds[i]['rois']) == 0:
+                continue
+
+            imgs[i] = imgs[i].copy()
+
+            for j in range(len(preds[i]['rois'])):
+                x1, y1, x2, y2 = preds[i]['rois'][j].astype(np.int)
+                obj = obj_list[preds[i]['class_ids'][j]]
+                score = float(preds[i]['scores'][j])
+                plot_one_box(imgs[i], [x1, y1, x2, y2], label=obj,score=score,color=color_list[get_index_label(obj, obj_list)])
+
+
+            if imshow:
+                cv2.imshow('img', imgs[i])
+                cv2.waitKey(0)
+
+            if imwrite:
+                cv2.imwrite(f'test/{name}_98.jpg', imgs[i])
+
+
+    out = invert_affine(framed_metas, out)
+    display(out, ori_imgs, imshow=False, imwrite=True)
+
+    # print('running speed test...')
+    # with torch.no_grad():
+    #     print('test1: model inferring and postprocessing')
+    #     print('inferring image for 10 times...')
+    #     t1 = time.time()
+    #     for _ in range(10):
+    #         _, regression, classification, anchors = model(x)
+
+    #         out = postprocess(x,
+    #                         anchors, regression, classification,
+    #                         regressBoxes, clipBoxes,
+    #                         threshold, iou_threshold)
+    #         out = invert_affine(framed_metas, out)
+
+    #     t2 = time.time()
+    #     tact_time = (t2 - t1) / 10
+    #     print(f'{tact_time} seconds, {1 / tact_time} FPS, @batch_size 1')
 
     # uncomment this if you want a extreme fps test
     # print('test2: model inferring only')
